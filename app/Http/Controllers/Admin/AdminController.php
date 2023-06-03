ef49e08d2ce4426b4620957bb54a61b7d833490a
okawa <0cu2045d@gmail.com>
2023-06-03 18:50:33 +0900

    編集画面のリンク先を動的に変更

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -13,16 +13,17 @@ use App\Services\Admin\CommonService;
 class AdminController extends Controller
 {
     protected $adminRepository;
-    protected $common;
+    protected $commonService;
 
-    public function __construct(AdminRepositoryInterface $adminRepository, CommonService $common)
+    public function __construct(AdminRepositoryInterface $adminRepository, CommonService $commonService)
     {
         $this->adminRepository = $adminRepository;
-        $this->common          = $common;
+        $this->commonService          = $commonService;
     }
 
     public function index(Request $request)
     {
+        session()->put('checkPointURL', $_SERVER['REQUEST_URI']);  //編集画面のリンク先を動的にするため
         $keywords = $request->only(['name', 'login_id', 'role', 'status']);
         $admins   = $this->adminRepository->searchPagination($keywords);
         return view('admin.admin.index', compact('admins', 'keywords'));
@@ -30,7 +31,8 @@ class AdminController extends Controller
 
     public function show(Request $request, $id)
     {
-        $admin = $this->adminRepository->find($id);
+        session()->put('checkPointURL', $_SERVER['REQUEST_URI']);  //編集画面のリンク先を動的にするため
+        $admin = $this->adminRepository->findOrFail($id);
         return view('admin.admin.show', compact('admin'));
     }
 
@@ -43,7 +45,7 @@ class AdminController extends Controller
     {
         $mode          = 'store';
         $inputs        = $request->all();
-        $maskPassword  = $this->common->maskPassword($inputs['password']);
+        $maskPassword  = $this->commonService->maskPassword($inputs['password']);
         unset($inputs['token']);
         return view('admin.admin.confirm', compact('mode', 'inputs', 'maskPassword'));
     }
@@ -61,7 +63,7 @@ class AdminController extends Controller
 
     public function edit($id)
     {
-        $admin = $this->adminRepository->find($id);
+        $admin = $this->adminRepository->findOrFail($id);
         return view('admin.admin.edit', compact('admin'));
     }
 
--- app/Repositories/Admin/Admin/AdminRepositoryInterface.php
+++ app/Repositories/Admin/Admin/AdminRepositoryInterface.php
@@ -10,7 +10,7 @@ interface AdminRepositoryInterface
 
     public function update($id, array $data);
 
-    public function find($id);
+    public function findOrFail($id);
 
     public function delete($id);
 
